18ed4eb403d3f2a9564efad3f9a0cba19000aefe
Albertojserr <alberto.j.serrano@gmail.com>
2021-10-15 16:00:13 +0200

    Solucionando errores

--- #Delaracion de Clases.py
+++ #Delaracion de Clases.py
@@ -1,5 +1,5 @@
 #Delaracion de Clases
- 
+
 class Operacion:
 
     def sumar(self,a,b): #la palabra self  debe ponerse en todo método.
@@ -19,11 +19,11 @@ class Operacion:
         return c;
 
 #Programa Principal
-print "Programa que hace operaciones con dos numeros";
+print ("Programa que hace operaciones con dos numeros");
 op = Operacion();
 a=10;
 b=4;
-print "Suma: "+str(a)+"+"+str(b)+"="+str(op.sumar(a,b));
-print "Resta: "+str(a)+"-"+str(b)+"="+str(op.restar(a,b));
-print "Multiplicación: "+str(a)+"*"+str(b)+"="+str(op.multiplicar(a,b));
-print "División: "+str(a)+"/"+str(b)+"="+str(op.dividir(a,b));
+print ("Suma: "+str(a)+"+"+str(b)+"="+str(op.sumar(a,b)));
+print ("Resta: "+str(a)+"-"+str(b)+"="+str(op.restar(a,b)));
+print ("Multiplicación: "+str(a)+"*"+str(b)+"="+str(op.multiplicar(a,b)));
+print ("División: "+str(a)+"/"+str(b)+"="+str(op.dividir(a,b)));
